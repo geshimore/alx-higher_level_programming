513000ab15e880ee03823694c23615a2b498b03b
root <peter.ngugi@mail.com>
2021-05-08 17:33:41 +0000

    Python - More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not roman_string:
-        return None
-    num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    total = 0
-    prev_val = 0
-    for i in range(len(roman_string)):
-        for key, val in num.items():
-            if key == roman_string[i]:
-                total += val
-                if prev_val < val:
-                    total -= (prev_val * 2)
-                    prev_val = val
-    return total
+    if type(roman_string) is not str or roman_string is None:
+        return 0
+
+    roman_letters = [
+        ['M', 1000], ['D', 500], ['C', 100], ['L', 50],
+        ['X', 10], ['V', 5], ['I', 1]
+    ]
+    num = 0
+    last = 0
+
+    for letter in roman_string:
+        for elem in roman_letters:
+            if letter == elem[0]:
+                if last == 0 or last >= elem[1]:
+                    num += elem[1]
+                elif last < elem[1]:
+                    num += elem[1] - (last * 2)
+
+                last = elem[1]
+
+    return num
