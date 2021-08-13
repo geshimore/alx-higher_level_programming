72a95ecc44d96810404d6a976398a9d5b583961d
root <peter.ngugi@mail.com>
2021-08-13 15:41:16 +0000

    return all table values

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,25 +1,25 @@
- 1#!/usr/bin/python3
- 2"""
- 3return all table values (table 'states')
- 4parameters given to script: username, password, database
- 5"""
- 6
- 7import MySQLdb
- 8from sys import argv
- 9
-10if __name__ == "__main__":
-11
-12    # connect to database
-13    db = MySQLdb.connect(host="localhost",
-14                         port=3306,
-15                         user=argv[1],
-16                         passwd=argv[2],
-17                         db=argv[3])
-18
-19    # create cursor to exec queries using SQL
-20    cursor = db.cursor()
-21    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-22    for row in cursor.fetchall():
-23        print(row)
-24    cursor.close()
-25    db.close()
+#!/usr/bin/python3
+"""
+return all table values (table 'states')
+parameters given to script: username, password, database
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+
+    # connect to database
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+
+    # create cursor to exec queries using SQL
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    for row in cursor.fetchall():
+        print(row)
+    cursor.close()
+    db.close()
