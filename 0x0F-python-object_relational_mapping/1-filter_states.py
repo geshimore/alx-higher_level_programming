caab036ad786336a0a8ce766b0d2d1b6cf6aebf7
root <peter.ngugi@mail.com>
2021-08-13 15:30:59 +0000

    Script that lists all states with a name starting with N

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,32 +1,16 @@
 #!/usr/bin/python3
-"""Script that lists all states with a name starting with N"""
+"""Lists all states from hbtn_0e_0_usa with Name starting with N"""
+
 import MySQLdb
-import sys
-
-
-def get_states_n():
-    """Takes arguments argv to list from database
-    Only lists with states that start with N
-    Arguments:
-        argv[1]: mysql username
-        argv[2]: mysql password
-        argv[3]: database name
-    """
-    db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=sys.argv[1],
-                         passwd=sys.argv[2],
-                         db=sys.argv[3])
-
-    cur = db.cursor()
-
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id")
-    rows = cur.fetchall()
-    for i in rows:
-        print(i)
-
-    cur.close()
-    db.close()
+from sys import argv
 
 if __name__ == "__main__":
-    get_states_n()
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("select * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
