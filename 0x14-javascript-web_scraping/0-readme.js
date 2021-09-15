da8eb16a450df8fb233e6d575aad10137252d857
geshimore <peter.ngugi@mail.com>
2021-09-15 19:31:18 +0000

    Web scraping updated

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -9,3 +9,4 @@ fs.readFile(filename, 'utf8', function (err, data) {
     console.log(data);
   }
 });
+1-writeme.js 100-starwars_characters.js 2-statuscode.js 3-starwars_title.js 4-starwars_count.js 5-request_store.js 6-completed_tasks.js
